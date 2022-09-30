cbfe2eba34ceee50040347a595f65c1283449908
Patrick <patrick@godknows.de>
2022-09-30 10:41:18 +0200

    Add project files.

---
+++ DotNet7BlazorDataBinding/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+using DotNet7BlazorDataBinding.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet7BlazorDataBinding.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 50).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Get([FromBody] string searchText)
+        {
+            var forecasts = Enumerable.Range(1, 50).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            });
+
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                return forecasts
+                    .Where(f => f.Summary.ToLower().Contains(searchText.ToLower()))
+                    .ToArray();
+            }
+
+            return forecasts.ToArray();
+        }
+    }
+}
